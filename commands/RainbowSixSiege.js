93a68da1f76361731fb9c54cf372a91afddb061f
ingoldxc1090 <32990371+ingoldxc1090@users.noreply.github.com>
2017-11-16 15:01:57 -0500

    converted to webhook

--- commands/RainbowSixSiege.js
+++ commands/RainbowSixSiege.js
@@ -1,13 +1,8 @@
 let http = require('http');
-let Discord = require('discord.js');
-let config = require('../config.json');
-exports.run = (client) => {
-    let gameChannel = client.channels.find("name", config.gameChannel);
-
-    client.user
-        .setAvatar("https://res.cloudinary.com/teepublic/image/private/s--W43hugIb--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1478457254/production/designs/784128_1.jpg")
-        .setUsername("Rainbow Six Siege Updates");
-
+let request = require('request');
+let fs = require('fs');
+let commandData = require("./commandData/RainbowSixSiege.json");
+exports.run = () => {
     http.get('http://steamcommunity.com/games/359550/rss/', (resp) => { //Makes get request to cat api page
         let data = '';
         resp.on('data', (chunk) => { //Writes data chunks to variable as they are recieved
@@ -21,34 +16,52 @@ exports.run = (client) => {
             var url = urlAr.split('</guid>')[0];
             var descAr = post.split('<description>')[1];
             var description = descAr.split('</description>')[0];
-            if(description.includes('<img')) {
-                var image = description.split('\"')[1]
+            if(description.includes('&lt;img')) {
+                var image = description.split('&quot;')[1]
             } else {
                 var imgDat = data.split('<image>')[1];
                 var imgAr = imgDat.split('<url>')[1];
                 var image = imgAr.split('</url>')[0];
             }
-            description.replace("<i>", "*");
-            description.replace("</i>", "*");
-            description.replace("<br>", "\\n");
-            descriptionFormat = description.split('<');
-            for(i = 0, i < descriptionFormat.length; i++) {
-                var formatRemover = descriptionFormat[i].split('>');
-                delete formatRemover[0]
-                descriptionFormat[i] = formatRemover.join();
+            descriptionFormat = description.split('&lt;');
+            for(i = 0; i < descriptionFormat.length; i++) {
+                var formatRemover = descriptionFormat[i].split('&gt;');
+                if(formatRemover[0] == "br") descriptionFormat[i] = "\n" + formatRemover[1];
+                else if(formatRemover[0] == "i") descriptionFormat[i] = "*" + formatRemover[1];
+                else if(formatRemover[0] == "/i") descriptionFormat[i] = "*" + formatRemover[1];
+                else descriptionFormat[i] = formatRemover[1];
             }
-            description = descriptionFormat.join();
-            const embed = new Discord.RichEmbed()
-                .setTitle(title)
-                .setDescription(description)
-                .setURL(url)
-                .setImage(image); //Sets extracted url as image for embed
-            gameChannel.send(embed);
+            description = descriptionFormat.join('');
+            text = "*" + title + "*" + description + "\n\n<" + url + ">\n\n" + image;
+            if(text.length > 2000){
+                text = "*" + title + "*" + description;
+                var textAr = text.match(new RegExp('.{1,' + 1994 + '}', 'g'));
+                textAr.push("<" + url + ">\n\n" + image);
+                textAr[0] = textAr[0] + "...";
+                for(i=1; i < textAr.length - 1; i++){
+                    textAr[i] = "..." + textAr[i] + "...";
+                }
+            }
+            if(title != commandData.embeds.title) {
+                commandData.embeds.title = title;
+                commandData.embeds.description = description;
+                commandData.embeds.url = url;
+                commandData.embeds.image = image;
+                fs.writeFile("./commandData/RainbowSixSiege", JSON.stringify(commandData), (err) => console.error);
+                for(i=0; i<textAr.length; i++) {
+                    request({
+                        method: 'POST',
+                        url: "https://discordapp.com/api/webhooks/365976300227133451/vHh1VYi7_2KknvO4fBBvrBlEDNBX9i34xNiAvItJRBoiQc3Mum3nDsftcHSiA1Uhj2B-",
+                        json: {
+                            "username": "Rainbow Six Siege Updates",
+                            "avatar_url": "https://res.cloudinary.com/teepublic/image/private/s--W43hugIb--/t_Preview/b_rgb:191919,c_limit,f_jpg,h_630,q_90,w_630/v1478457254/production/designs/784128_1.jpg",
+                            "content": textAr[i]
+                        }
+                    });
+                }
+            }
         });
     }).on("error", (err) => {
         console.error(err);
     });
-    client.user
-        .setAvatar("https://i.imgur.com/q0JhzFa.png")
-        .setUsername("News Bot");
 }
---
+++ webhook.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+
+fs.readdir("./commands", function(err, files) {
+    if(err){
+        console.error(err);
+        return;
+    }
+    let command = require(`./commands/${files[1]}`);
+    command.run();
+    for(i=1; i > files.length; i++) {
+        let command = require(`./commands/${files[i]}`);
+        try {
+            command.run(client);
+        } catch(err){
+            console.error(err);
+        }
+    }
+});
+timer();
+function timer () {
+    setTimeout(function () {
+        fs.readdir("./commands", function(err, files) {
+            if(err){
+                console.error(err);
+                return;
+            }
+            for(i=0; i > files.length; i++) {
+                let command = require(`./command/${files[i]}`);
+                try {
+                    command.run(client);
+                } catch(err){
+                    console.error(err);
+                }
+            }
+        });
+        timer()
+    }, 300000)
+}
